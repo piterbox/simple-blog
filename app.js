020448a7d699a426d5c8352623274ebfa33fa44f
Peter Klekot <peter@lotemx.com>
2018-05-09 21:24:01 +0300

    start app dev

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('', (req, res)=>{
+	res.send('It works!');
+});
+
+const port = process.env.PORT | 4000;
+
+app.listen(port, () => {
+	console.log(`Start application on port: ${port}`);
+});
